2d3bb6764c9ed34df31e0153f67e2a8b5bb1a663
milesgranger <miles59923@gmail.com>
2021-10-10 16:28:07 +0200

    defautllib and set python arch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from Cython.Distutils import build_ext
 if os.getenv("RUNNER_OS", "").lower() == "windows":
     p = pathlib.Path(__file__).absolute().parent.joinpath("target").joinpath("release").joinpath("flaco.lib")
     assert p.is_file(), "Rust lib not built!"
-    extra_link_args = [f"/LIBPATH:{p.parent}", f"/link:{p}"]  # .lib on MSVC .a on MinGW
+    extra_link_args = [f"/LIBPATH:{p.parent}", f"/DEFAULTLIB:flaco.lib"]  # .lib on MSVC .a on MinGW
 else:
     extra_link_args = ["-l:libflaco.a"]
 
