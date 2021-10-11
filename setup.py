765b1e5ebc54f923210b16b9151bdb91a34dcf4d
milesgranger <miles59923@gmail.com>
2021-10-11 09:37:01 +0200

    set subsystem

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ if os.getenv("RUNNER_OS", "").lower() == "windows":
     p = pathlib.Path(__file__).parent.joinpath("target").joinpath("release").joinpath("flaco.lib")
     assert p.is_file(), "Rust lib not built!"
     extra_link_args = [f"/link", f"/LIBPATH:D:\\a\\flaco\\flaco\\target\\release\\flaco.lib"]  # .lib on MSVC .a on MinGW
-    extra_compile_args = [f"/link", f"/LIBPATH:D:\\a\\flaco\\flaco\\target\\release\\flaco.lib"]
+    extra_compile_args = [f"/link", f"/LIBPATH:D:\\a\\flaco\\flaco\\target\\release\\flaco.lib",
+                          "/SUBSYSTEM:WINDOWS"]
 else:
     extra_link_args = ["-l:libflaco.a"]
     extra_compile_args = ["-fopenmp", "-O3"]
