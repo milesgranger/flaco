0558c54a00413f5cade4dcbb8c0e232f58df76bb
milesgranger <miles59923@gmail.com>
2021-10-12 13:19:31 +0200

    Add a couple static libs to link to

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 extension = Extension(
     name="*",
     sources=[str(pathlib.Path("flaco/*.pyx"))],
-    libraries=["flaco"],
+    libraries=["flaco", "mswsock", "ntdll", "ws2_32"],
     include_dirs=[np.get_include(), str(pathlib.Path(__file__).absolute().parent.joinpath("flaco"))],
     library_dirs=["D:\\a\\flaco\\flaco\\target\\release"],
     extra_compile_args=extra_compile_args,
