7d0180e1b27af64e8848f31bfcee084a4e5cbcc5
milesgranger <miles59923@gmail.com>
2021-10-13 18:19:19 +0200

    Value comparison test

--- tests/test_general.py
+++ tests/test_general.py
@@ -63,3 +63,21 @@ def test_simple_timing(postgresdb_engine, postgresdb_connection_uri, simple_tabl
 
     # faster than pandas by at least 1/3rd
     assert t1 > t2 * 0.66
+
+def test_simple_group_by(postgresdb_engine, postgresdb_connection_uri, simple_table):
+    df1 = pd.read_sql_table(simple_table, con=postgresdb_engine)
+
+    with Database(postgresdb_connection_uri) as con:
+        data = read_sql(f"select * from {simple_table}", con)
+    df2 = pd.DataFrame(data, copy=False)
+
+    # just numeric columns
+    df1_group = df1.groupby("col1").sum()
+    df2_group = df2.groupby("col1").sum()
+    assert df1_group.equals(df2_group)
+
+    # max will include string columns
+    df1_group = df1.groupby("col1").max()
+    df2_group = df2.groupby("col1").max()
+    assert df1_group.equals(df2_group)
+
