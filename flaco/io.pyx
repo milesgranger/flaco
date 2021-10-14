31c52000ff593503012c5dca86acf09b9bee8cc8
milesgranger <miles59923@gmail.com>
2021-10-14 13:28:46 +0200

    working, but no handling of resize via numpy c-api

--- flaco/io.pyx
+++ flaco/io.pyx
@@ -3,6 +3,9 @@
 
 cimport numpy as np
 import numpy as np
+import array
+from cpython cimport array
+from cpython.object cimport PyObject
 from libc.stdlib cimport malloc
 from flaco cimport includes as lib
 
@@ -93,8 +96,16 @@ cpdef dict read_sql(str stmt, Database db, int n_rows=-1):
 
     return {columns[i]: output[i] for i in range(columns.shape[0])}
 
-cdef void resize(np.ndarray array, np.uint32_t len):
-    array.resize(len, refcheck=False)
+cdef void resize(np.ndarray arr, np.uint32_t len):
+    cdef int refcheck = 0
+    cdef array.array dims_arr = array.array('L', [len])
+    cdef np.PyArray_Dims dims;
+    cdef PyObject *ret
+    dims.ptr = <np.npy_intp*>&dims_arr.data.as_ints[0]
+    dims.len = 1
+    # TODO: potential error!
+    np.PyArray_Resize(arr, &dims, refcheck, np.NPY_ANYORDER)
+
 
 cdef np.ndarray array_init(lib.Data data, int len):
     cdef np.ndarray array
