e53e624c162cad372192f4baefed09d6825eb01c
milesgranger <miles59923@gmail.com>
2021-10-25 17:10:06 +0200

    keep connectorx in benchmarks

--- benchmarks/test_benchmarks.py
+++ benchmarks/test_benchmarks.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 import pandas as pd
+import connectorx as cx
 from memory_profiler import profile
 from sqlalchemy import create_engine
 from flaco.io import Database, read_sql
@@ -26,7 +27,7 @@ DB_TABLES = (
 )
 
 
-@pytest.mark.parametrize("loader", ("pandas", "flaco"))
+@pytest.mark.parametrize("loader", ("pandas", "flaco", "connectorx"))
 def test_basic(benchmark, loader: str):
     def _read_all_tables(func, sql, con):
         for table in DB_TABLES:
@@ -35,30 +36,37 @@ def test_basic(benchmark, loader: str):
             # as numpy arrays default to no copy.
             if loader == "pandas":
                 func(sql.format(table=table), con)
+            elif loader == "connectorx":
+                func(DB_URI, sql.format(table=table))
             else:
-                pd.DataFrame(func(sql.format(table=table), con))
+                pd.DataFrame(func(sql.format(table=table), con), copy=False)
 
     if loader == "pandas":
         engine = create_engine(DB_URI)
         benchmark(
             _read_all_tables, pd.read_sql, "select * from {table}", engine,
         )
+    elif loader == "connectorx":
+        benchmark(
+            _read_all_tables, cx.read_sql,"select * from {table}", DB_URI
+        )
     else:
         with Database(DB_URI) as con:
             benchmark(_read_all_tables, read_sql, "select * from {table}", con)
 
 
-@pytest.mark.parametrize("loader", ("pandas", "flaco"))
+@pytest.mark.parametrize("loader", ("pandas", "flaco", "connectorx"))
 @pytest.mark.parametrize(
     "n_rows", np.arange(100_000, 1_000_000, 100_000), ids=lambda val: f"rows={val}"
 )
 def test_incremental_size(benchmark, loader: str, n_rows: int):
-
     table = _table_setup(n_rows=n_rows, include_nulls=False)
 
     if loader == "pandas":
         engine = create_engine(DB_URI)
         benchmark(lambda *args: pd.read_sql(*args), f"select * from {table}", engine)
+    elif loader == "connectorx":
+        benchmark(cx.read_sql, DB_URI, f"select * from {table}", return_type="pandas")
     else:
         with Database(DB_URI) as con:
             benchmark(
@@ -83,7 +91,8 @@ def _table_setup(n_rows: int = 1_000_000, include_nulls: bool = False):
             col6 bytea,
             col7 date,
             col8 timestamp without time zone,
-            col9 timestamp with time zone
+            col9 timestamp with time zone,
+            col10 time
         )
     """)
 
@@ -97,6 +106,7 @@ def _table_setup(n_rows: int = 1_000_000, include_nulls: bool = False):
     df["col7"] = pd.date_range('2000-01-01', '2001-01-01', periods=len(df))
     df["col8"] = pd.to_datetime(df.col7)
     df["col9"] = pd.to_datetime(df.col7, utc=True)
+    df["col10"] = df.col9.dt.time
     df.to_sql(table, index=False, con=engine, chunksize=10_000, if_exists="append")
 
     if include_nulls:
@@ -109,7 +119,6 @@ def _table_setup(n_rows: int = 1_000_000, include_nulls: bool = False):
 def memory_profile():
     stmt = "select * from test_table"
 
-    import connectorx as cx
     _cx_df = cx.read_sql(DB_URI, stmt, return_type="pandas")
 
     with Database(DB_URI) as con:
@@ -120,5 +129,5 @@ def memory_profile():
     _pandas_df = pd.read_sql(stmt, engine)
 
 if __name__ == "__main__":
-    #_table_setup(n_rows=500_000, include_nulls=False)
+    _table_setup(n_rows=500_000, include_nulls=False)
     memory_profile()
