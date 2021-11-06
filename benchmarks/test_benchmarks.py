1cc5be0b835f7769a2451d3e97cae6f550017e4b
milesgranger <miles59923@gmail.com>
2021-11-06 13:06:26 +0100

    Better with datetime64, better mem worse speed than connectorx

--- benchmarks/test_benchmarks.py
+++ benchmarks/test_benchmarks.py
@@ -131,9 +131,8 @@ def memory_profile():
 
 if __name__ == "__main__":
     #_table_setup(n_rows=1_000_000, include_nulls=False)
-    stmt = "select * from test_table"
-
-    with Database(DB_URI) as con:
-        data = read_sql(stmt, con)
-        _flaco_df = pd.DataFrame(data, copy=False)
-    #memory_profile()
+    #stmt = "select * from test_table"
+    #with Database(DB_URI) as con:
+    #    data = read_sql(stmt, con)
+    #    _flaco_df = pd.DataFrame(data, copy=False)
+    memory_profile()
