fdda3787eb1c2520f2552702819113e7959be8e8
Miles Granger <miles59923@gmail.com>
2022-09-01 07:33:56 +0200

    Add Column::push

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,10 @@ impl Column {
     pub fn inner_mut<T: Any + 'static>(&mut self) -> &mut T {
         self.array.as_any_mut().downcast_mut::<T>().unwrap()
     }
+    pub fn push<V, T: array::TryPush<V> + Any + 'static>(&mut self, value: V) -> Result<()> {
+        self.inner_mut::<T>().try_push(value)?;
+        Ok(())
+    }
 }
 
 pub fn read_sql(client: &mut pg::Client, sql: &str) -> Result<Table> {
@@ -60,19 +64,16 @@ pub fn read_sql(client: &mut pg::Client, sql: &str) -> Result<Table> {
 fn init_table(row: &pg::Row) -> Result<Vec<Column>> {
     let mut table = Vec::with_capacity(row.len());
     for column in row.columns() {
+        let name = column.name();
         let col = match column.type_() {
-            &pg::types::Type::BYTEA => {
-                Column::new(array::MutableBinaryArray::<i32>::new(), column.name())
-            }
-            &pg::types::Type::CHAR => {
-                Column::new(array::MutablePrimitiveArray::<i8>::new(), column.name())
-            }
-            &pg::types::Type::INT2 => {
-                Column::new(array::MutablePrimitiveArray::<i16>::new(), column.name())
-            }
-            &pg::types::Type::INT4 => {
-                Column::new(array::MutablePrimitiveArray::<i32>::new(), column.name())
-            }
+            &Type::BYTEA => Column::new(array::MutableBinaryArray::<i32>::new(), name),
+            &Type::BOOL => Column::new(array::MutableBooleanArray::new(), name),
+            &Type::CHAR => Column::new(MutablePrimitiveArray::<i8>::new(), name),
+            &Type::INT2 => Column::new(MutablePrimitiveArray::<i16>::new(), name),
+            &Type::INT4 => Column::new(MutablePrimitiveArray::<i32>::new(), name),
+            &Type::INT8 => Column::new(MutablePrimitiveArray::<i64>::new(), name),
+            &Type::FLOAT4 => Column::new(MutablePrimitiveArray::<f32>::new(), name),
+            &Type::FLOAT8 => Column::new(MutablePrimitiveArray::<f64>::new(), name),
             _ => todo!(),
         };
         table.push(col)
@@ -80,16 +81,16 @@ fn init_table(row: &pg::Row) -> Result<Vec<Column>> {
     Ok(table)
 }
 
-macro_rules! append_row {
-    ($column:ident, $row:ident, $idx:ident, $dtype:ty) => {
-        $column
-            .inner_mut::<array::MutablePrimitiveArray<$dtype>>()
-            .push($row.get::<_, Option<$dtype>>($idx))
-    };
-}
-
 #[inline(always)]
 fn append_row(table: &mut Vec<Column>, row: &pg::Row) -> Result<()> {
+    macro_rules! append_row {
+        ($column:ident, $row:ident, $idx:ident, $dtype:ty) => {
+            $column
+                .inner_mut::<array::MutablePrimitiveArray<$dtype>>()
+                .push($row.get::<_, Option<$dtype>>($idx))
+        };
+    }
+
     for (idx, column) in table.iter_mut().enumerate() {
         match column.dtype() {
             &DataType::Binary => {
@@ -97,9 +98,17 @@ fn append_row(table: &mut Vec<Column>, row: &pg::Row) -> Result<()> {
                     .inner_mut::<array::MutableBinaryArray<i32>>()
                     .push(row.get::<_, Option<Vec<u8>>>(idx));
             }
+            &DataType::Boolean => {
+                column
+                    .inner_mut::<array::MutableBooleanArray>()
+                    .push(row.get::<_, Option<bool>>(idx));
+            }
             &DataType::Int8 => append_row!(column, row, idx, i8),
-            //&DataType::Int16 => append_row!(i16),
-            //&DataType::Int32 => append_row!(i32),
+            &DataType::Int16 => append_row!(column, row, idx, i16),
+            &DataType::Int32 => append_row!(column, row, idx, i32),
+            &DataType::Int64 => append_row!(column, row, idx, i64),
+            &DataType::Float32 => append_row!(column, row, idx, f32),
+            &DataType::Float64 => append_row!(column, row, idx, f64),
             _ => todo!(),
         }
     }
