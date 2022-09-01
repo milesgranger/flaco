fa8a47c61126590082d6b004a0d41d0799775370
Miles Granger <miles59923@gmail.com>
2022-09-01 13:20:48 +0200

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,15 @@ use postgres::fallible_iterator::FallibleIterator;
 use postgres::types::{FromSql, Type};
 use postgres::RowIter;
 use rust_decimal::prelude::{Decimal, ToPrimitive};
-use std::any::Any;
 use std::collections::HashMap;
 use std::error::Error;
 use std::iter::Iterator;
 use std::net::IpAddr;
+use std::{any::Any, collections::BTreeMap};
 use time;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-pub type Table = Vec<Column>;
+pub type Table = BTreeMap<String, Column>;
 
 pub struct Column {
     array: Box<dyn Array>,
@@ -50,13 +50,7 @@ impl Column {
 
 pub fn read_sql(client: &mut pg::Client, sql: &str) -> Result<Table> {
     let mut row_iter = client.query_raw::<_, &i32, _>(sql, &[])?;
-    let mut table = if let Some(row) = row_iter.next()? {
-        let mut table = init_table(&row)?;
-        append_row(&mut table, &row)?;
-        table
-    } else {
-        return Err("Query returned no rows!".into());
-    };
+    let mut table = BTreeMap::new();
     while let Some(row) = row_iter.next()? {
         append_row(&mut table, &row);
     }
@@ -64,55 +58,94 @@ pub fn read_sql(client: &mut pg::Client, sql: &str) -> Result<Table> {
 }
 
 #[inline(always)]
-fn init_table(row: &pg::Row) -> Result<Vec<Column>> {
-    let mut table = Vec::with_capacity(row.len());
-    for column in row.columns() {
-        let name = column.name();
-        let col = match column.type_() {
-            &Type::BYTEA => Column::new(MutableBinaryArray::<i32>::new(), name),
-            &Type::BOOL => Column::new(MutableBooleanArray::new(), name),
-            &Type::CHAR => Column::new(MutablePrimitiveArray::<i8>::new(), name),
-            &Type::INT2 => Column::new(MutablePrimitiveArray::<i16>::new(), name),
-            &Type::INT4 => Column::new(MutablePrimitiveArray::<i32>::new(), name),
-            &Type::INT8 => Column::new(MutablePrimitiveArray::<i64>::new(), name),
-            &Type::FLOAT4 => Column::new(MutablePrimitiveArray::<f32>::new(), name),
-            &Type::FLOAT8 => Column::new(MutablePrimitiveArray::<f64>::new(), name),
-            &Type::TIMESTAMP => Column::new(MutablePrimitiveArray::<i64>::new(), name),
-            &Type::TIMESTAMPTZ => Column::new(MutablePrimitiveArray::<i64>::new(), name),
-            &Type::DATE => Column::new(MutablePrimitiveArray::<i32>::new(), name),
-            &Type::VARCHAR | &Type::CHAR_ARRAY | &Type::TEXT | &Type::NAME | &Type::UNKNOWN => {
-                Column::new(MutableBinaryArray::<i32>::new(), name)
-            }
-            _ => todo!(),
-        };
-        table.push(col)
-    }
-    Ok(table)
-}
-
-#[inline(always)]
-fn append_row(table: &mut Vec<Column>, row: &pg::Row) -> Result<()> {
-    for (idx, (row_column, table_column)) in row.columns().iter().zip(table.iter_mut()).enumerate()
-    {
+fn append_row(table: &mut BTreeMap<String, Column>, row: &pg::Row) -> Result<()> {
+    for (idx, row_column) in row.columns().iter().enumerate() {
+        let column_name = row_column.name().to_string();
         match row_column.type_() {
             &Type::BYTEA => {
-                table_column
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutableBinaryArray::<i32>::new(), row_column.name())
+                    })
                     .push::<_, MutableBinaryArray<i32>>(row.get::<_, Option<Vec<u8>>>(idx))?;
             }
             &Type::BOOL => {
-                table_column.push::<_, MutableBooleanArray>(row.try_get(idx).ok())?;
-                table_column.dtype = DataType::Boolean;
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| Column::new(MutableBooleanArray::new(), row_column.name()))
+                    .push::<_, MutableBooleanArray>(row.try_get(idx).ok())?;
             }
             &Type::CHAR => {
-                table_column.push::<_, MutablePrimitiveArray<i8>>(row.try_get(idx).ok())?;
-                table_column.dtype = DataType::Int8;
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<i8>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<i8>>(row.try_get(idx).ok())?;
+            }
+            &Type::TEXT | &Type::VARCHAR | &Type::UNKNOWN | &Type::NAME => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutableBinaryArray::<i32>::new(), row_column.name())
+                    })
+                    .push::<_, MutableBinaryArray<i32>>(row.try_get::<_, Vec<u8>>(idx).ok())?;
+            }
+            &Type::INT2 => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<i16>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<i16>>(row.try_get(idx).ok())?;
+            }
+            &Type::INT4 => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<i32>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<i32>>(row.try_get(idx).ok())?;
+            }
+            &Type::INT8 => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<i64>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<i64>>(row.try_get(idx).ok())?;
+            }
+            &Type::FLOAT4 => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<f32>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<f32>>(row.try_get(idx).ok())?;
+            }
+            &Type::FLOAT8 => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(MutablePrimitiveArray::<f64>::new(), row_column.name())
+                    })
+                    .push::<_, MutablePrimitiveArray<f64>>(row.try_get(idx).ok())?;
+            }
+            &Type::TIMESTAMP => {
+                table
+                    .entry(column_name)
+                    .or_insert_with(|| {
+                        Column::new(
+                            MutablePrimitiveArray::<i64>::new()
+                                .to(DataType::Time64(TimeUnit::Microsecond)),
+                            row_column.name(),
+                        )
+                    })
+                    .push::<_, MutablePrimitiveArray<i64>>(row.try_get(idx).ok())?;
             }
             _ => todo!(),
-        };
+        }
     }
     Ok(())
-}
-
-fn column_names(row: &pg::Row) -> impl Iterator<Item = String> + '_ {
-    row.columns().iter().map(|c| c.name().to_string())
 }
